67e2cb468e1d573e48ce0cfb38510c3e0107d5fb
Szczepaniak-M <m.szczepaniak99@gmail.com>
2021-12-07 19:29:39 +0100

    Add Liquibase example

---
+++ spring-data/src/test/java/pl/org/akai/springdata/SpringDataApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.org.akai.springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(true);
+    }
+
+}
